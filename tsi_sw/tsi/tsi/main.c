ee35d5a6a785762a92f77e146a832efb31f58ef3
nessa <nessa>
2017-04-03 14:31:52 -0400

    moved some of the software stuff into atmel studio project

---
+++ tsi_sw/tsi/tsi/main.c
@@ -0,0 +1,18 @@
+/*
+ * tsi.c
+ *
+ * Created: 4/3/2017 2:17:40 PM
+ * Author : nessa
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
