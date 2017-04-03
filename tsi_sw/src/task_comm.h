d3ee90c216f880a1b8c4400ca3a0dad4d3b9a099
nessa <nessa@localhost.localdomain>
2017-04-03 12:03:11 -0400

    copied over pacman software, made inital tasks

---
+++ tsi_sw/src/task_comm.h
@@ -0,0 +1,16 @@
+/*
+ * Communication Thread for TSI board
+ */
+#include "atmel/can_lib.h"
+#include "atmel/can_drv.h"
+#include "params.h"
+#include "atomutils.h"
+//#include "i2c.h"
+
+/* #ifndef PACK_NUM */
+/* #define PACK_NUM 0x003 */
+/* #endif */
+
+
+void task_can_init(void);
+void task_can(uint32_t data);
