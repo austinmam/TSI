fc047ab0304e75df1f0ff39dcf7c4e157a45b06b
nessa <nessa@localhost.localdomain>
2017-04-10 12:13:07 -0400

    altered pacman software so applicable to tsib

---
+++ tsib_pfu/src/task_heartbeat.h
@@ -0,0 +1,10 @@
+#ifndef TASK_HEARTBEAT_H
+#define TASK_HEARTBEAT_H
+
+#include <avr/io.h>
+#include <stdint.h>
+#include "config.h"
+
+void task_heartbeat(uint32_t data);
+
+#endif /*TASK_HEARTBEAT_H*/
