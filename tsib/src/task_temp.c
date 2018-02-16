24bf2baa0e3a869cafcbdefc3aed94332b5b761b
austinmam <austinmam@users.noreply.github.com>
2018-02-15 20:53:35 -0500

    temp conversion formula

--- tsib/src/task_temp.c
+++ tsib/src/task_temp.c
@@ -16,13 +16,13 @@ void temp_init(void) {
 	SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0);
 };
 
+//NOTE ABOUT THE TEMP MEASUREMENT
 //This method needs to convert the 16 bits into a readable temp
 //First two bits are always 1
 //Third bit is the LSB of the temp which is 2^-5 or 0.03125
 //After that, each bit is the next power of 2 (ex. 4th bit is 2^-4, 5th bit is 2^-3)
-int16_t temp_conversion(uint16_t data){
-
-}
+//After looking into the temp, the conversion from the temp sensor to a temperature is
+// temp = ((temp_sensor_value - 3)/3200)*25
 
 //Task to check the temperature reading
 void task_temp(void) {
@@ -32,7 +32,7 @@ void task_temp(void) {
 		//Wait for transmission complete
 		while(!(SPSR & (1<<SPIF)));
 		//Returns the new value in the data register which should be temp
-		tempature = SPDR; //set the global variable temperature so we can send it(from the params.h file)
+		temperature = ((SPDR - 3)/3200)*25; //set the global variable temperature so we can send it(from the params.h file)
 		atomTimerDelay(100);
 	}	
 };
