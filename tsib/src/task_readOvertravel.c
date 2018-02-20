bd7fba1ac599006bce46d705b2b42c79f0e20b09
thomasclagett <35735885+thomasclagett@users.noreply.github.com>
2018-02-20 10:42:51 -0500

    Add files via upload

--- tsib/src/task_readOvertravel.c
+++ tsib/src/task_readOvertravel.c
@@ -1,8 +1,16 @@
 #include "task_readOvertravel.h"
 
 void overtravel_init(void) {
+
+	//initial value for overtravel
+	//0 for not pushed, 1 for pushed
 	overtravel = 0x00;
+
+	//set pin PE6 as input
 	DDRE |= (0 << PE6);
+
+	//enable pull-up resistor
+	PORTE |= (1 << PE6);
 }
 
 void task_readOvertravel(uint32_t data) {
