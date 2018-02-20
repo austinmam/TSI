6c5675cc03afb7c3e06686c84d0c8e126877e4af
thomasclagett <35735885+thomasclagett@users.noreply.github.com>
2018-02-20 10:41:47 -0500

    Add files via upload

--- task_readOvertravel.c
+++ task_readOvertravel.c
@@ -1,8 +1,15 @@
-
 #include "task_readOvertravel.h"
 
 void overtravel_init(void) {
-	overtravel = false;
+
+	//initial value for overtravel
+	//0 for not pushed, 1 for pushed
+	overtravel = 0x00;
+
+	//set pin PE6 as input
+	DDRE |= (0 << PE6);
+
+	//enable pull-up resistor
 	PORTE |= (1 << PE6);
 }
 
@@ -10,8 +17,8 @@ void task_readOvertravel(uint32_t data) {
 	overtravel_init();
 
 	for(;;) {
-		if((PINE & (1 << PB6))) { // Read Brake Overtravel
-			overtravel = true;
+		if((PINE & (1 << PE6))) { // Read Brake Overtravel
+			overtravel = 0x01;
 		}
 		
 		atomTimerDelay(50);
