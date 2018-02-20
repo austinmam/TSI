2471691d7d825cdfdfe05c1589d1b84082631132
thomasclagett <35735885+thomasclagett@users.noreply.github.com>
2018-02-20 08:51:37 -0500

    Add files via upload

--- task_readOvertravel.c
+++ task_readOvertravel.c
@@ -1,16 +1,16 @@
 #include "task_readOvertravel.h"
 
 void overtravel_init(void) {
-	overtravel = false;
-	PORTE |= (1 << PE6);
+	overtravel = 0x00;
+	DDRE |= (0 << PE6);
 }
 
 void task_readOvertravel(uint32_t data) {
 	overtravel_init();
 
 	for(;;) {
-		if((PINE & (1 << PB6))) { // Read Brake Overtravel
-			overtravel = true;
+		if((PINE & (1 << PE6))) { // Read Brake Overtravel
+			overtravel = 0x01;
 		}
 		
 		atomTimerDelay(50);
