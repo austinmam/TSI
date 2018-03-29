a4487198cd3f68ccd258fc16f2a52ac7b58e062f
thomasclagett <35735885+thomasclagett@users.noreply.github.com>
2018-03-29 14:36:31 -0400

    syntax fixes

--- tsib/src/task_readCurrent.h
+++ tsib/src/task_readCurrent.h
@@ -12,7 +12,7 @@ uint8_t i_ch;
 uint16_t currVolt;
 
 void i_adcinit(void);
-uint16_t recalculate(uint16_t currRead);
+uint16_t recalculate_curr(uint16_t currRead);
 void task_readCurrent(uint32_t data);
 
 #endif /*TASK_READCURRENT_H*/
