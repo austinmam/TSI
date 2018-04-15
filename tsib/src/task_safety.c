1f8f3f2425d988f04a33e94903f26f78e15a0c26
Austin Mam <austincmam@gmail.com>
2018-04-15 16:32:17 -0400

    Added additional logic to prevent going into drive mode when scada drop signal is 1.

--- tsib/src/task_safety.c
+++ tsib/src/task_safety.c
@@ -95,8 +95,10 @@ void task_safety(uint32_t data) {
 						if(!(PINE & (1 << PE5))) {    			//Check Safety Loop is closed
 							if(!(PINB & (1 << PB5))) {  		//Check Throttle Plausibility
 								if(appsReading < THROTTLE_ON) { //Throttle must be below 0.5V
-									state = SETUP_DRIVE;
-									buttonPushed = 0;
+									if(throttle_control == 0){	//Drop out of drive signal from SCADA must be low
+										state = SETUP_DRIVE;
+										buttonPushed = 0;
+									}
 								}
 							}	
 						}
