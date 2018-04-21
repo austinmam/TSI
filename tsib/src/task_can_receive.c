76979a073106f5166a6de7f6b99b02c2157ef8b3
Austin Mam <austincmam@gmail.com>
2018-04-21 17:22:56 -0400

    Took out can receive

--- tsib/src/task_can_receive.c
+++ tsib/src/task_can_receive.c
@@ -14,25 +14,25 @@ void task_can_receive(uint32_t data){
 
 	for(;;){	
 
-		for(int i = 0; i<7; i = i + 1){
-			can_buff_receive[i] = 0x00;//clear the buffer of previous data if any
-		}
+		// for(int i = 0; i<7; i = i + 1){
+		// 	can_buff_receive[i] = 0x00;//clear the buffer of previous data if any
+		// }
 
-		can_frame_receive.id.std = SCADA_SRC_ID;//maybe the source id is different
-		can_frame_receive.ctrl.ide = 0;
-		can_frame_receive.ctrl.rtr = 0;
-		can_frame_receive.dlc = 1;
-		can_frame_receive.cmd = CMD_RX;
+		// can_frame_receive.id.std = SCADA_SRC_ID;//maybe the source id is different
+		// can_frame_receive.ctrl.ide = 0;
+		// can_frame_receive.ctrl.rtr = 0;
+		// can_frame_receive.dlc = 1;
+		// can_frame_receive.cmd = CMD_RX;
 
-		while(can_cmd(&can_frame_receive) != CAN_CMD_ACCEPTED);
-		atomTimerDelay(500);//wait for 5 seconds to see if message arrived
+		// while(can_cmd(&can_frame_receive) != CAN_CMD_ACCEPTED);
+		// atomTimerDelay(500);//wait for 5 seconds to see if message arrived
 
-		if(can_get_status(&can_frame_receive)== CAN_STATUS_COMPLETED){
-			throttle_control = can_buff_receive[0];
-		}else{//if not received in 5 seconds, abort the frame
-			can_frame_receive.cmd = CMD_ABORT;
-			while(can_cmd(&can_frame_receive) != CAN_CMD_ACCEPTED);
-		}
+		// if(can_get_status(&can_frame_receive)== CAN_STATUS_COMPLETED){
+		// 	throttle_control = can_buff_receive[0];
+		// }else{//if not received in 5 seconds, abort the frame
+		// 	can_frame_receive.cmd = CMD_ABORT;
+		// 	while(can_cmd(&can_frame_receive) != CAN_CMD_ACCEPTED);
+		// }
 
 		//For testing whether message can be received or not
 		//Turns on and off an LED based on message
