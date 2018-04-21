a46df1367a3cb1ed7c1ec0a21b02a9580ffedb01
Austin Mam <austincmam@gmail.com>
2018-04-21 18:17:38 -0400

    Changed apps reading condition to >=

--- tsib/src/task_safety.c
+++ tsib/src/task_safety.c
@@ -100,7 +100,7 @@ void task_safety(uint32_t data) {
 					if(!(PINB & (1 << PB4))) { 		  			//Check brake pressed
 						if(!(PINE & (1 << PE5))) {    			//Check Safety Loop is closed
 						 	if(!(PINB & (1 << PB5))) {  		//Check Throttle Plausibility
-						 		if(appsReading < THROTTLE_ON) { //Throttle must be below 0.5V
+						 		if(appsReading <= THROTTLE_ON) { //Throttle must be below 0.5V
 						 			//if(throttle_control == 0){	//Drop out of drive signal from SCADA must be low
 										state = SETUP_DRIVE;
 										buttonPushed = 0;
